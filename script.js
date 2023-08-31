94132afe06b1eff8326655d9c07b0281432dfe07
“altharaa” <aletheamaureen01@gmail.com>
2023-09-01 00:23:53 +0700

    code formatting

--- script.js
+++ script.js
@@ -1,71 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const matrixSize = 3; 
-    const addBtn = document.getElementById("addBtn");
-    const subtractBtn = document.getElementById("subtractBtn");
-
-    function createMatrixInput(tableId) {
-        const table = document.getElementById(tableId);
-        table.innerHTML = '';
-
-        for (let i = 0; i < matrixSize; i++) {
-            const row = document.createElement("tr");
-            for (let j = 0; j < matrixSize; j++) {
-                const cell = document.createElement("td");
-                const input = document.createElement("input");
-                input.type = "number";
-                input.id = `${tableId}${i}${j}`;
-                cell.appendChild(input);
-                row.appendChild(cell);
-            }
-            table.appendChild(row);
-        }
+const matrixA = [
+    [document.getElementById("matrixA00"), document.getElementById("matrixA01"), document.getElementById("matrixA02")],
+    [document.getElementById("matrixA10"), document.getElementById("matrixA11"), document.getElementById("matrixA12")],
+    [document.getElementById("matrixA20"), document.getElementById("matrixA21"), document.getElementById("matrixA22")]
+  ];
+  
+  const matrixB = [
+    [document.getElementById("matrixB00"), document.getElementById("matrixB01"), document.getElementById("matrixB02")],
+    [document.getElementById("matrixB10"), document.getElementById("matrixB11"), document.getElementById("matrixB12")],
+    [document.getElementById("matrixB20"), document.getElementById("matrixB21"), document.getElementById("matrixB22")]
+  ];
+  
+  const resultMatrix = [
+    [document.getElementById("result00"), document.getElementById("result01"), document.getElementById("result02")],
+    [document.getElementById("result10"), document.getElementById("result11"), document.getElementById("result12")],
+    [document.getElementById("result20"), document.getElementById("result21"), document.getElementById("result22")]
+  ];
+  
+  function addMatrices() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const sum = parseFloat(matrixA[i][j].value) + parseFloat(matrixB[i][j].value);
+        resultMatrix[i][j].value = sum;
+      }
     }
-
-    createMatrixInput("matrixA");
-    createMatrixInput("matrixB");
-
-    addBtn.addEventListener("click", performOperation("add"));
-    subtractBtn.addEventListener("click", performOperation("subtract"));
-
-    document.getElementById("resetBtn").addEventListener("click", function () {
-        createMatrixInput("matrixA");
-        createMatrixInput("matrixB");
-        document.getElementById("resultMatrix").innerHTML = '';
-    });
-
-    function performOperation(operation) {
-        return function() {
-            const resultMatrix = [];
-            for (let i = 0; i < matrixSize; i++) {
-                resultMatrix.push([]);
-                for (let j = 0; j < matrixSize; j++) {
-                    const a = parseFloat(document.getElementById(`matrixA${i}${j}`).value);
-                    const b = parseFloat(document.getElementById(`matrixB${i}${j}`).value);
-                    let result = 0;
-                    if (operation === "add") {
-                        result = a + b;
-                    } else if (operation === "subtract") {
-                        result = a - b;
-                    }
-                    resultMatrix[i].push(result);
-                }
-            }
-            displayResultMatrix(resultMatrix);
-        };
+  }
+  
+  function subtractMatrices() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const difference = parseFloat(matrixA[i][j].value) - parseFloat(matrixB[i][j].value);
+        resultMatrix[i][j].value = difference;
+      }
     }
-
-    function displayResultMatrix(resultMatrix) {
-        const table = document.getElementById("resultMatrix");
-        table.innerHTML = '';
-
-        for (let i = 0; i < matrixSize; i++) {
-            const row = document.createElement("tr");
-            for (let j = 0; j < matrixSize; j++) {
-                const cell = document.createElement("td");
-                cell.textContent = resultMatrix[i][j];
-                row.appendChild(cell);
-            }
-            table.appendChild(row);
-        }
+  }
+  
+  function resetMatrices() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        matrixA[i][j].value = "";
+        matrixB[i][j].value = "";
+        resultMatrix[i][j].value = "";
+      }
     }
-});
+  }
+  document.getElementById("addBtn").addEventListener("click", addMatrices);
+  document.getElementById("subtractBtn").addEventListener("click", subtractMatrices);
+  document.getElementById("resetBtn").addEventListener("click", resetMatrices);
